2e5c07970db31ab0b1fee6af674d402dd52c068e
Waleed Amjad <waleedamjad99@hotmail.com>
2022-06-14 11:43:03 +0500

    Add Edit/delete/ Task Funtion

--- js/crudFuntions.js
+++ js/crudFuntions.js
@@ -2,6 +2,7 @@ const inputNewTodo = document.getElementById('input-new-task');
 const taskContainer = document.getElementById('Task-Container');
 const targetDataDiv = document.getElementById('show-items-qty');
 const delBtn = document.querySelectorAll('.delete-btn');
+const errorField = document.getElementById('Display-Error');
 
 let todo = JSON.parse(localStorage.getItem('Tasks')) || [];
 
@@ -29,13 +30,14 @@ const display = () => {
                   />
                 </div>
                 <div class="btn-container">
-                  <span class="del-btn material-symbols-outlined"> edit </span>
+                  <span class="del-btn material-symbols-outlined edit-btn" id=""edit${j}> edit </span>
                   <span class="del-btn material-symbols-outlined delete-btn" id=del${j}> delete </span>
                 </div>
               </li>`;
   });
   registerElements();
   targetDataDiv.innerHTML = `${todo.length} Items Left`;
+  // errorField.innerHTML = '';
 };
 
 const reAssignIndex = () => {
@@ -53,6 +55,7 @@ const registerElements = () => {
     //   const kebabMenu = document.querySelectorAll('.vertical-dots');
     //   const doneBtn = document.querySelectorAll('.done-btn');
     const delBtn = document.querySelectorAll('.delete-btn');
+    const editBtn = document.querySelectorAll('.edit-btn');
 
     //   checkboxes.forEach((box) => {
     //     box.addEventListener('click', this.updateCompleteTask.bind(this));
@@ -64,17 +67,17 @@ const registerElements = () => {
     //     done.addEventListener('click', this.saveChanges.bind(this));
     //   });
 
-    targetTaskField.forEach((input) => {
-      input.addEventListener('click', () => {
-        const targetBtn = input.target;
-        const targetParent = targetBtn.parentElement;
-        targetParent.classList.add('dot-menu');
-        targetParent.children[0].children[1].disabled = false;
-        targetParent.children[1].style.display = 'none';
-        targetParent.children[3].style.display = 'block';
-        targetParent.children[2].style.display = 'block';
-      });
-    });
+    // targetTaskField.forEach((input) => {
+    //   input.addEventListener('click', () => {
+    //     const targetBtn = input.target;
+    //     const targetParent = targetBtn.parentElement;
+    //     targetParent.classList.add('dot-menu');
+    //     targetParent.children[0].children[1].disabled = false;
+    //     targetParent.children[1].style.display = 'none';
+    //     targetParent.children[3].style.display = 'block';
+    //     targetParent.children[2].style.display = 'block';
+    //   });
+    // });
 
     delBtn.forEach((del) => {
       del.addEventListener('click', () => {
@@ -93,10 +96,41 @@ const registerElements = () => {
       });
     });
     //   dragging();
+
+    targetTaskField.forEach((task) => {
+      task.addEventListener('keypress', (e) => {
+        if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+          task.disabled = true;
+          console.log(task);
+          task.parentElement.parentElement.classList.remove('editable-task');
+          todo.forEach((n) => {
+            if (task.id === `Task${n.index}`) {
+              n.description = task.value;
+            }
+          });
+          populateLocalStorage();
+          display();
+        }
+      });
+    });
+
+    editBtn.forEach((edit) => {
+      edit.addEventListener('click', () => {
+        const editBtn = edit;
+        let targetInput =
+          editBtn.parentElement.parentElement.children[0].children[1];
+        targetInput.disabled = false;
+        editBtn.parentElement.parentElement.classList.add('editable-task');
+        // populateLocalStorage();
+        // display();
+      });
+    });
   }
 };
 
-const add = (description, completed = false, index = (i += 1)) => {
+let k = todo.length;
+
+const add = (description, completed = false, index = (k += 1)) => {
   todo.push({ description, completed, index });
   populateLocalStorage();
   display();
@@ -104,10 +138,18 @@ const add = (description, completed = false, index = (i += 1)) => {
 
 inputNewTodo.addEventListener('keypress', (e) => {
   if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+    if(inputNewTodo.value.length > 0 )
+    {
     add(inputNewTodo.value);
     inputNewTodo.value = '';
-    console.log(todo);
+    }
+    else {
+      errorField.innerHTML = 'Input Field cannot be empty!!';
+      setTimeout(() => {
+        errorField.innerHTML = '';
+      }, 5000);
   }
+}
 });
 
 display();
